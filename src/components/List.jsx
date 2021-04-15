f0248de2885a6e61da014df65ab55fe384298420
Timothy Wang <timothy.wang@clsa.com>
2021-04-15 12:52:17 +0800

    Add pull to regresh

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { FlatList } from "react-native";
 import ListItem from "./ListItem";
 
-const List = ({ data, onEndReached, pageSize = 10 }) => (
+const List = ({
+  data,
+  onEndReached,
+  onRefresh,
+  pageSize = 10,
+  refreshing = false,
+}) => (
   <FlatList
     data={data}
     initialNumToRender={pageSize}
     keyExtractor={(item) => `${item.id}`}
+    refreshing={refreshing}
     renderItem={({ item }) => <ListItem item={item} />}
     onEndReached={onEndReached}
     onEndReachedThreshold={0.5}
+    onRefresh={onRefresh}
   />
 );
-
 export default List;
--- src/screens/ArticleListScreen.jsx
+++ src/screens/ArticleListScreen.jsx
@@ -1,18 +1,17 @@
-import { useQuery } from "@apollo/client";
-import _isEmpty from "lodash-es/isEmpty";
-import React, { useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-} from "react-native";
+import { NetworkStatus, useQuery } from "@apollo/client";
+import React, { useEffect, useState, useMemo, useCallback } from "react";
+
+import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import { ActivityIndicator } from "react-native-paper";
 import Background from "../components/Background";
 import List from "../components/List";
 import ListHeader from "../components/ListHeader";
 import { FETCH_ARTICLES } from "../helpers/graphql";
 
 const styles = StyleSheet.create({
+  activityIndicator: {
+    marginVertical: 10,
+  },
   listContainer: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
@@ -27,35 +26,74 @@ const ArticleList = ({
   // const [articles, setArticles] = useState([]);
   // const [nextArticles, setNextArticles] = useState([]);
   const [page, setPage] = useState(0);
-  const [pageSize, setPageSize] = useState(10);
+  const [pageSize, setPageSize] = useState(5);
+  const [refreshing, setRefreshing] = useState(false);
 
-  const { data: { articles } = {}, fetchMore, loading: isLoading } = useQuery(
-    FETCH_ARTICLES,
-    {
-      variables: { limit: pageSize, start: page * pageSize },
-      onError: (error) => {
+  const {
+    data: { articles = [] } = {},
+    fetchMore = () => {},
+    loading: isLoading,
+    refetch,
+    networkStatus,
+  } = useQuery(FETCH_ARTICLES, {
+    variables: { limit: pageSize, start: page * pageSize },
+    notifyOnNetworkStatusChange: true,
+    onError: ({ message }) => {
+      console.error(`Error: ${message}`);
+    },
+  });
+
+  useEffect(() => {
+    const fetchData = async (page) => {
+      try {
+        console.log("page", page);
+        if (page) {
+          await fetchMore({
+            variables: { limit: pageSize, start: page * pageSize },
+          });
+        } else {
+          setRefreshing(true);
+          await refetch({
+            variables: { limit: pageSize, start: 0 },
+          });
+          setRefreshing(false);
+        }
+      } catch (error) {
         console.log("error", error);
-        console.error(`Error: ${error.message}`);
-      },
-    }
-  );
+      }
+    };
+    fetchData(page);
+  }, [fetchMore, page, pageSize, refetch]);
 
   return (
     <Background>
-      <ListHeader title="Asia Morning Line" />
+      {/* <ListHeader title="Asia Morning Line" /> */}
+      <ListHeader
+        title={`Asia Morning Line - ${
+          networkStatus === NetworkStatus.fetchMore
+            ? `Fetching ${page} page`
+            : networkStatus === NetworkStatus.refetch
+            ? "Refetching"
+            : `${articles.length} Articles Loaded`
+        }`}
+      />
       <SafeAreaView style={styles.listContainer}>
         <List
           data={articles}
           pageSize={pageSize}
+          refreshing={refreshing}
           onEndReached={() => {
-            const nextPage = page + 1;
-            setPage(nextPage);
-            fetchMore({
-              variables: { limit: pageSize, start: nextPage * pageSize },
-            });
+            setPage(page + 1);
+          }}
+          onRefresh={() => {
+            setPage(0);
           }}
         />
-        {isLoading ? <ActivityIndicator /> : null}
+        <ActivityIndicator
+          hidesWhenStopped
+          animating={isLoading}
+          style={styles.activityIndicator}
+        />
       </SafeAreaView>
     </Background>
   );
